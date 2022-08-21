f4542c9d4949fb3f2265ef06fec89ed1631ea907
seyfi <giraykut01@mail.com>
2022-08-21 13:02:23 +0300

    main branch

--- src/pazar1/gitpazar1.java
+++ src/pazar1/gitpazar1.java
@@ -4,5 +4,6 @@ public class gitpazar1 {
     public static void main(String[] args) {
         System.out.println("pazar calısması ");
         System.out.println("seyfi branch da calışma yapılıyor");
+        System.out.println("main branch da değişliklik yapıldı");
     }
 }
