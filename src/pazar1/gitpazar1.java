092ddc4be3714e3c67eaea95d003a68791083984
seyfi <giraykut01@mail.com>
2022-08-21 13:09:01 +0300

    merge

--- src/pazar1/gitpazar1.java
+++ src/pazar1/gitpazar1.java
@@ -5,5 +5,7 @@ public class gitpazar1 {
         System.out.println("pazar calısması ");
         System.out.println("seyfi branch da calışma yapılıyor");
         System.out.println("main branch da değişliklik yapıldı");
+        System.out.println("pazarcalışması merge olarak birleştirildi ve seyfi de değişiklikyapıldı");
+
     }
 }
