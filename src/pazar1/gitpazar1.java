438783111f96ae96ce12f773a20722cd83591350
seyfi <giraykut01@mail.com>
2022-08-21 12:48:23 +0300

    seyfi branch

--- src/pazar1/gitpazar1.java
+++ src/pazar1/gitpazar1.java
@@ -3,5 +3,6 @@ package pazar1;
 public class gitpazar1 {
     public static void main(String[] args) {
         System.out.println("pazar calısması ");
+        System.out.println("seyfi branch da calışma yapılıyor");
     }
 }
